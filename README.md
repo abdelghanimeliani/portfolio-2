f3aee38642392ccd5197f41f0a9f6a0296d85e33
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-07-30 18:10:03 +0200

    update READMEé

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-a remplir
+# MY PORTFOLIO
+
+## c'est quoi ?
+c'est un site statique a pour but de me representer au clients , entreprises
+<br />
+## captures
+|hero Section|
+|:------------:|
+![hero Section](./assets/img/screen.png)|
+
+
+
+## Built With
+* [HTML]
+* [Bootstrap](https://getbootstrap.com/)
+
--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
  
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'ka_meliani@esi.dz';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -19,14 +19,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'esi.dz',
+    'username' => 'ka_meliani',
+    'password' => 'abgmelesi03101902',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
